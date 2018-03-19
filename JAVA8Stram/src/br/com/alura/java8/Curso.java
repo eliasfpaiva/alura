5fd45ca20933b1d631862390ac9015e5e7f40af5
Elias Paiva <elias.paiva@IDEAGRI.LAN>
2018-03-19 16:08:09 -0300

    Atualizando cursos

---
+++ JAVA8Stram/src/br/com/alura/java8/Curso.java
@@ -0,0 +1,22 @@
+package br.com.alura.java8;
+
+public class Curso {
+
+	private String nome;
+	private int alunos;
+	
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public int getAlunos() {
+		return this.alunos;
+	}
+	
+	
+}
---
+++ JAVA8Stram/src/br/com/alura/java8/testeStream.java
@@ -0,0 +1,51 @@
+package br.com.alura.java8;
+
+import java.util.ArrayList;
+//import java.util.Comparator;
+import java.util.List;
+//import java.util.stream.Stream;
+
+public class testeStream {
+
+	public static void main(String[] args) {
+		
+		List<Curso> cursos = new ArrayList<Curso>();
+		
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Java 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		cursos.stream()
+		   .filter(c -> c.getAlunos() > 50)
+		   .map(Curso::getAlunos)
+		   .forEach(System.out::println);
+		
+//		cursos.stream()
+//		   .filter(c -> c.getAlunos() > 50)
+//		   .map(c -> c.getAlunos())
+//		   .forEach(x -> System.out.println(x));
+		
+//		Stream<String> nomes = cursos.stream().map(Curso::getNome);
+//		
+//		nomes.forEach(System.out::println);
+		
+//		cursos.stream().filter(c -> c.getAlunos() >50).forEach(c -> System.out.println(c.getNome()));
+		
+//		cursos.forEach(c -> System.out.println(c.getNome()));
+		
+//		cursos.sort(Comparator.comparing(Curso::getAlunos));
+		
+//		cursos.sort(Comparator.comparing(c -> c.getAlunos()));
+		
+//		cursos.sort(new Comparator<Curso>() {
+//
+//			@Override
+//			public int compare(Curso c1, Curso c2) {
+//				return c1.getAlunos() - c2.getAlunos();
+//			}
+//		});
+		
+//		cursos.forEach(c -> System.out.println(c.getNome()));
+	}
+}
