1fe12e31eadd0ccf3dcaa5b472a6c9c8e8916de5
Elias Paiva <elias.paiva@IDEAGRI.LAN>
2018-03-29 16:04:11 -0300

    Login

--- JAVA WEB Júnior/gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ JAVA WEB Júnior/gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -9,17 +9,34 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns="/*")
 public class FiltroDeAuditoria implements Filter{
 
 	@Override
-	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
 		HttpServletRequest request = (HttpServletRequest) req;
 		String uri = request.getRequestURI();
-		System.out.println("Usuário acessando URI: " + uri);
+		String usuario = getUsuario(request);
+		System.out.println("Usuário: " + usuario + " acessando URI: " + uri);
 		chain.doFilter(req, resp);
+	}
+
+	private String getUsuario(HttpServletRequest request) {
+		String usuario = "<deslogado>";
+		
+		
+		Cookie[] cookies = request.getCookies();
+		if(cookies == null)	
+			return usuario;
+		for (Cookie cookie : cookies) {
+			if(cookie.getName().equals("usuario.logado"))
+				usuario = cookie.getValue();
+		}
+		return usuario;
 	}
 
 	@Override
