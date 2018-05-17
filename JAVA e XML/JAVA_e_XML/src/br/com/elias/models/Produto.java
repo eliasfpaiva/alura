96b53a20b1ecc4c7bc3fc2808adce5c24357b0e1
elias.paiva <elias.paiva@ideagri.com.br>
2018-05-17 13:18:24 -0300

    Atualizando

--- JAVA e XML/JAVA_e_XML/src/br/com/elias/models/Produto.java
+++ JAVA e XML/JAVA_e_XML/src/br/com/elias/models/Produto.java
@@ -4,22 +4,16 @@ public class Produto {
 
 	private String nome;
 	private Double preco;
-
+                         
 	public Produto(String nome, Double preco) {
 		this.nome = nome;
 		this.preco = preco;
 	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public Double getPreco() {
-		return preco;
-	}
-
+	
+	public String getNome() { return nome; }
+                         
+	public Double getPreco() { return preco; }
+                         
 	@Override
-	public String toString() {
-		return "Produto [nome=" + nome + ", preco=" + preco + "]";
-	}
+	public String toString() { return "Produto [nome=" + nome + ", preco=" + preco + "]"; }
 }
