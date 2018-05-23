05ca000264df3bc568b5f333b00f28991669d535
elias.paiva <elias.paiva@ideagri.com.br>
2018-05-23 16:06:11 -0300

    Atualizando

--- Produtividade com o IntelliJ/src/br/com/elias/bytebank/Principal.java
+++
@@ -1,8 +0,0 @@
-package br.com.elias.bytebank;
-
-public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Bem-vindo ao Bytebank!");
-        Funcionario funcionario = new Funcionario();
-    }
-}
