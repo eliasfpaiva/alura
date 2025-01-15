3bc6462779a1d045f850347b6362236a5d7febf1
Elias Paiva <eliasfpaiva@gmail.com>
2025-01-15 12:10:05 -0300

    Refatoração

--- SERPRO/Kotlin/AluGames/src/main/kotlin/principal/Main.kt
+++ SERPRO/Kotlin/AluGames/src/main/kotlin/principal/Main.kt
@@ -4,7 +4,6 @@ import servicos.ConsumoApi
 import java.util.*
 
 fun main(){
-    val scanner = Scanner(System.`in`)
     println(ConsumoApi().buscaJogadores())
     println(ConsumoApi().buscaJogos())
 }
