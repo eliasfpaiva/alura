f1881dafe9491594022f06b1c3a2b5b90eafd0e6
Elias Paiva <eliasfpaiva@gmail.com>
2025-01-17 18:38:48 -0300

    fix: extensão de string para quando estiver vazia

--- SERPRO/Kotlin/AluGames/src/main/kotlin/Utils/ExtensaoString.kt
+++ SERPRO/Kotlin/AluGames/src/main/kotlin/Utils/ExtensaoString.kt
@@ -5,6 +5,7 @@ import java.time.Period
 import java.time.format.DateTimeFormatter
 
 fun String.transformarEmIdade(): Int {
+    if (this.isEmpty()) return 0
     val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
     val dataNascimento = LocalDate.parse(this, formatter)
     return Period.between(dataNascimento, LocalDate.now()).years
